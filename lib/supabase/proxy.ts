38147be16c9c57432b6a34507a85593d7ff480be
siendomigueldev-DESKTOP <siendomigueldev@gmail.com>
2026-01-21 18:13:43 -0500

    Fix error auth

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -51,7 +51,8 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !/^[a-zA-Z0-9]+$/.test(request.nextUrl.pathname.slice(1)) // Allow short URLs
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
